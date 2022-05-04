14bbb0dce17236348592df43a776298d09afbacc
jannatul114 <jfmithila114@gmail.com>
2022-05-04 14:40:23 +0600

    items inserted

--- index.js
+++ index.js
@@ -12,6 +12,35 @@ app.use(express.json());
 //OizTQOmb568WN0uV
 //allFruits
 
+//fruits
+//B7quVO9XMUTOCc1H
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xjjrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const fruitsCollection = client.db("inventoryItems").collection("item");
+
+
+        app.get('/allfruits', async (req, res) => {
+            const quary = {};
+            const cursor = fruitsCollection.find(quary);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 
 
