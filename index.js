29abefc631bfe1335cc0c686bac36f5dd445c22d
jannatul114 <jfmithila114@gmail.com>
2022-05-07 00:56:23 +0600

    code updated

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ app.use(cors())
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xjjrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -20,19 +35,18 @@ async function run() {
 
 
         //jwt token implementing
-        // app.post('/login', async (req, res) => {
-        //     const user = req.body;
-        //     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        //         expiresIn: '1d'
-        //     })
-        //     res.send({ accessToken })
-        // })
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken })
+        })
 
 
         //getting all item
         app.get('/allfruits', async (req, res) => {
-            authHeader = req.headers.authorization;
-            console.log(authHeader);
+
             const quary = {};
             const cursor = fruitsCollection.find(quary);
             const result = await cursor.toArray();
@@ -55,6 +69,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/usersfruits/:id', async (req, res) => {
+            const id = req.params.id;
+            const quary = { _id: ObjectId(id) }
+            const result = await fruitsCollection.deleteOne(quary);
+            res.send(result);
+        })
+
         //posting items
         app.post('/allfruits', async (req, res) => {
             const newItem = req.body;
@@ -64,12 +85,20 @@ async function run() {
 
 
         //getting items
-        app.get('/usersfruits', async (req, res) => {
+        app.get('/usersfruits', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const quary = { email: email };
-            const cursor = fruitsCollection.find(quary);
-            const result = await cursor.toArray();
-            res.send(result);
+            if (email === decodedEmail) {
+                const quary = { email: email };
+                const cursor = fruitsCollection.find(quary);
+                const result = await cursor.toArray();
+                res.send(result);
+            }
+
+            else {
+                res.status(403).send({ message: 'Forbidden access' })
+            }
+
         })
         //posting user items
         app.post('/usersfruits', async (req, res) => {
